2fb79c4ca58bbb9e3f33ab8e6803649b56e08428
ag37354@ubt-uni.net <ag37354@ubt-uni.net>
2021-03-14 23:49:00 +0100

    added and worked on page not found files and secret page file

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,21 @@
+<template>
+<ErrorView :title="'Ooops page you are trying to access doesn\'t exist'" 
+            :status="404" 
+            :navigateToPage="'HomePage'" 
+            :navigateToText="'Navigate back Home'"/>
+</template>
+
+<script>
+import ErrorView from '../components/ErrorView'
+export default {
+    components:{
+        ErrorView
+    }   
+}
+</script>
+
+<style scoped>
+#btnMarginmeti{
+    margin:30px;
+}
+</style>
---
+++ src/pages/SecretPage.vue
@@ -0,0 +1,13 @@
+<template >
+    <h1>I am secret page</h1>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
