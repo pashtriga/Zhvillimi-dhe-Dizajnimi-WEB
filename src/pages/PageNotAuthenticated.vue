6332fa8d6bf72922b9029951b4fbfe2567252694
ag37354@ubt-uni.net <ag37354@ubt-uni.net>
2021-03-14 23:47:01 +0100

    added pageNotAuthenticated file

---
+++ src/pages/PageNotAuthenticated.vue
@@ -0,0 +1,21 @@
+<template>
+<ErrorView :title="'Ooops you are not authenticated to visit to this page,please login'" 
+            :status="'401'" 
+            :navigateToPage="'Login'" 
+            :navigateToText="'Navigate to Login Page'"/>
+</template>
+
+<script>
+import ErrorView from '../components/ErrorView'
+export default {
+    components:{
+        ErrorView
+    }   
+}
+</script>
+
+<style scoped>
+#btnMarginmeti{
+    margin:30px;
+}
+</style>
