a524794479e59f0de7eb1ecb9f5a5609ea3e9f36
ag37354@ubt-uni.net <ag37354@ubt-uni.net>
2021-03-14 23:45:30 +0100

    worked on pages like categoriesList and homepage

---
+++ src/pages/categoriesList.vue
@@ -0,0 +1,47 @@
+/* eslint-disable vue/valid-template-root */
+<template>
+    <section>
+        <h2>List of Categories</h2>
+        <br>
+        <router-link :to="{name : 'CreateCategories' }" >Create new Category</router-link>
+        <br>
+        <br>
+        <div class="container maxwidth">
+             <CategoryItem v-for="cat in categories" :key="cat.name" :category="cat"/>
+        </div>
+        <br>
+        <br>
+    </section>      
+</template>
+
+<script>
+import axios from 'axios'
+import CategoryItem from '../components/categories/categoryItem'
+export default {
+    components:{
+        CategoryItem
+    },
+    data(){
+        return {
+            categories:[]
+        }
+    },
+    created(){
+        axios.get('/api/v1/categories')
+        .then(res => {
+           // debugger
+            this.categories = res.data
+        })
+    }
+    
+}
+</script>
+
+
+<style scoped>
+
+.maxwidth{
+    width: 400px;
+}
+
+</style>
---
+++ src/pages/homepage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Home page meti</h1>
+        <ul>
+            <li v-for="item in categories" v-bind:key="item.id"> {{ item.name }} </li> 
+          
+        </ul>
+         <div class="form-group">
+                  <button type="button" class="btn btn-primary">Primary</button>
+                </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return {
+            categories:[]
+        }
+    },
+    created(){
+        axios.get('/api/v1/categories')
+        .then(res => {
+            debugger
+            this.categories = res.data
+        })
+    }
+    
+}
+</script>
